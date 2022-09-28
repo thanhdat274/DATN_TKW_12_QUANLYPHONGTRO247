796578dd6fd3bafe70f3a850e3d6460f18b9fcf5
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2022-09-28 16:58:11 +0700

    up

--- src/Layout/Manager/Landlords/index.tsx
+++ src/Layout/Manager/Landlords/index.tsx
@@ -1,30 +1,27 @@
-
 import { useRouter } from 'next/router';
 import { ReactNode, useState } from 'react';
 
 import SideBar from '@/components/Sidebar';
 import Navbar from '@/components/AdminNavbar';
-import AdminFooter from '@/components/AdminFooter';
+import { useUserContext } from '@/context/UserContext';
+import { CircleSpinnerOverlay } from 'react-spinner-overlay';
 
 export interface ILayoutAdminProps {
   children: ReactNode;
 }
 
 const LayoutLandlords = ({ children }: ILayoutAdminProps) => {
-  const [toggle, setToggle] = useState(false);
-  const changeToggle = () => {
-    setToggle(!toggle);
-  };
-  const router = useRouter();
+  const { loading } = useUserContext();
 
   return (
     <div>
+      {<CircleSpinnerOverlay loading={loading} color="#2563eb" size={100} message="Loadinggg" />}
+
       <SideBar />
       <Navbar />
       <div className="relative md:ml-64 bg-blueGray-100">
-        <div className="px-[8px] mx-auto w-full">
-          <div className='bg-gray-100 p-4'>{children}</div>
-          <AdminFooter />
+        <div className=" mx-auto w-full h-full">
+          <div className="bg-gray-100 p-4 min-h-screen">{children}</div>
         </div>
       </div>
     </div>
