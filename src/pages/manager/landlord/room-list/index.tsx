ac71eaebefc4fed57dfd30ff6ba16d39b74aaa0c
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2022-09-28 16:59:55 +0700

    Update index.tsx

--- src/pages/manager/landlord/room-list/index.tsx
+++ src/pages/manager/landlord/room-list/index.tsx
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faMoneyBill, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import axios from 'axios';
 import swal from 'sweetalert';
-
+import { useRouter } from 'next/router';
 type Props = {};
 
-const RoomList = (props: Props) => {
+const data = [
+  { name: 'phong so 1', status: 'phong trong' },
+  { name: 'phong so 2', status: 'phong trong' },
+  { name: 'phong so 3', status: 'phong trong' },
+  { name: 'phong so 4', status: 'phong trong' },
+  { name: 'phong so 5', status: 'phong trong' },
+  { name: 'phong so 6', status: 'phong trong' },
+  { name: 'phong so 7', status: 'phong trong' },
+];
+
+const ListRoom = (props: Props) => {
+  const router = useRouter();
+  const { id } = router.query;
   const [room, setRoom] = useState([]);
 
   useEffect(() => {
@@ -30,12 +42,11 @@ const RoomList = (props: Props) => {
       if (data) {
         setRoom(room.filter((item: any) => item.id !== id));
       }
-      swal("Bạn đã xóa thành công!",{
-        icon: "success",
+      swal('Bạn đã xóa thành công!', {
+        icon: 'success',
       });
     }
   };
-
   return (
     <div className="h-screen">
       <header className="bg-white shadow">
@@ -47,14 +58,11 @@ const RoomList = (props: Props) => {
               </h2>
             </div>
             <div className="mt-5 flex lg:mt-0 lg:ml-4">
-              <a href="room-list/add" className="sm:ml-3">
-                <button
-                  type="button"
-                  className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
+              <Link href={`/manager/landlord/${id}/list-room/add`}>
+                <a className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   Thêm mới
-                </button>
-              </a>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -64,84 +72,50 @@ const RoomList = (props: Props) => {
           <div className="flex flex-col">
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 align-middle inline-block min-w-full ">
-                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                  <table className="min-w-full divide-y divide-gray-200">
-                    <thead className="bg-gray-50">
-                      <tr>
-                        <th scope="col" className="px-9 py-3 text-center text-xs font-bold uppercase tracking-wider">
-                          STT
-                        </th>
-                        <th scope="col" className="px-9 py-3 text-center text-xs font-bold uppercase tracking-wider">
-                          tên phòng
-                        </th>
-                        <th scope="col" className="px-9 py-3 text-center text-xs font-bold uppercase tracking-wider">
-                          giá
-                        </th>
-                        <th scope="col" className="px-9 py-3 text-center text-xs font-bold uppercase tracking-wider">
-                          diện tích phòng
-                        </th>
-                        <th scope="col" className="px-9 py-3 text-center text-xs font-bold uppercase tracking-wider">
-                          số lượng người thuê
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-9 py-3 text-center text-xs font-bold uppercase tracking-wider"
-                        ></th>
-                      </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                      {room &&
-                        room.map((item: any, index) => {
-                          return (
-                            <tr key={index}>
-                              <td className="px-6 py-4 whitespace">
-                                <div className="text-center">{index + 1}</div>
-                              </td>
-                              <td className="px-6 py-4 whitespace">
-                                <div className="text-center">{item.name}</div>
-                              </td>
-                              <td className="px-6 py-4 whitespace">
-                                <div className="text-center">{item.price}</div>
-                              </td>
-                              <td className="px-6 py-4 whitespace">
-                                <div className="text-center">{item.room_size} m2</div>
-                              </td>
-                              <td className="px-6 py-4 whitespace">
-                                <div className="text-center">{item.people}</div>
-                              </td>
-                              <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="text-center flex">
-                                  <Link
-                                    href="/manager/landlord/room-list/1"
-                                    className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                  >
-                                    <a className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                      Thêm khách
-                                    </a>
-                                  </Link>
-                                  <Link
-                                    href={`/manager/landlord/room-list/${item.id}`}
-                                    className="text-amber-500 hover:text-amber-600 px-4 py-2"
-                                  >
-                                    <a className="text-amber-500 hover:text-amber-600 px-4 py-2">
-                                      <FontAwesomeIcon className="h-[20px]" icon={faPenToSquare}></FontAwesomeIcon>
-                                    </a>
-                                  </Link>
-                                  <button
-                                    onClick={() => {
-                                      remove(item.id);
-                                    }}
-                                    className="btn text-red-500 hover:text-red-600 px-4 py-2"
-                                  >
-                                    <FontAwesomeIcon className="h-[20px]" icon={faTrash}></FontAwesomeIcon>
-                                  </button>
-                                </div>
-                              </td>
-                            </tr>
-                          );
-                        })}
-                    </tbody>
-                  </table>
+                <div className="flex flex-wrap gap-[20px]">
+                  {room &&
+                    room.map((item: any, index) => {
+                      return (
+                        <div className="w-full max-w-[250px] border-2 p-[20px] bg-white rounded-[5px]" key={index}>
+                          <h2 className="text-xl flex items-center gap-2 mb-[20px]">
+                            <FontAwesomeIcon className="h-[15px]" icon={faHouse}></FontAwesomeIcon>
+                            {item.name}
+                          </h2>
+                          <Link
+                            href="/manager/landlord/room-renter/add"
+                            className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 inline-block"
+                          >
+                            <a className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 inline-block mb-[20px]">
+                              Thêm khách
+                            </a>
+                          </Link>
+
+                          <p className="flex items-center gap-2 mb-[20px]">
+                            <FontAwesomeIcon className="h-[15px]" icon={faMoneyBill}></FontAwesomeIcon>
+                            <span className="text-red-500"> {item.price}</span>
+                          </p>
+
+                          <div className="text-center flex gap-3">
+                            <Link
+                              href={`/manager/landlord/room-list/${item.id}`}
+                              className="text-amber-500 hover:text-amber-600"
+                            >
+                              <a className="text-amber-500 hover:text-amber-600">
+                                <FontAwesomeIcon className="h-[20px]" icon={faPenToSquare}></FontAwesomeIcon>
+                              </a>
+                            </Link>
+                            <button
+                              onClick={() => {
+                                remove(item.id);
+                              }}
+                              className="btn text-red-500 hover:text-red-600"
+                            >
+                              <FontAwesomeIcon className="h-[20px]" icon={faTrash}></FontAwesomeIcon>
+                            </button>
+                          </div>
+                        </div>
+                      );
+                    })}
                 </div>
               </div>
             </div>
@@ -152,4 +126,4 @@ const RoomList = (props: Props) => {
   );
 };
 
-export default RoomList;
+export default ListRoom;
