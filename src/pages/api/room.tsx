da6e6fe1419722123a8b3fc22d1cce03550a3beb
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2022-10-18 10:52:56 +0700

    Create room.tsx

---
+++ src/pages/api/room.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from 'react';
+import instance from './instance';
+
+export const listRoom = (id: any) => {
+  const a = JSON.parse(localStorage.getItem('user') as string);
+
+  const url = `list-room/${a.user._id}/${id}`;
+  return instance.get(url, {
+    headers: {
+      Authorization: `Bearer ${a.token}`,
+    },
+  });
+};
+
+export const addRoom = (data: any) => {
+  const a = JSON.parse(localStorage.getItem('user') as string);
+  const url = `/room/add`;
+  return instance.post(url, data, {
+    headers: {
+      Authorization: `Bearer ${a.token}`,
+    },
+  });
+};
+
+export const removeRoom = (id: number) => {
+  const a = JSON.parse(localStorage.getItem('user') as any);
+
+  const url = `room/remove/${id}`;
+  return instance.delete(url, {
+    headers: {
+      Authorization: `Bearer ${a.token}`,
+    },
+  });
+};
+export const readRoom = (id_room: string) => {
+  const a = JSON.parse(localStorage.getItem('user') as any);
+
+  const url = `room/${id_room}`;
+  return instance.get(url, {
+    headers: {
+      Authorization: `Bearer ${a.token}`,
+    },
+  });
+};
+export const updateRoom = (room: any) => {
+  const a = JSON.parse(localStorage.getItem('user') as any);
+
+  const url = `/room/update/${room._id}`;
+  return instance.put(url, room, {
+    headers: {
+      Authorization: `Bearer ${a.token}`,
+    },
+  });
+};
