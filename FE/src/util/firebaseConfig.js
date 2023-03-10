0e3f5c07477789de25772c7d597c3f543b3c110e
Nguyễn Thành Đạt <77159701+thanhdat274@users.noreply.github.com>
2023-03-10 17:06:12 +0700

    Update firebaseConfig.js

--- FE/src/util/firebaseConfig.js
+++ FE/src/util/firebaseConfig.js
@@ -3,10 +3,10 @@ import { getStorage } from "firebase/storage";
 
 // Initialize Firebase
 const app = initializeApp({
-    apiKey: "AIzaSyDioD326fD9ybiVMRFUBPTXNUw2MYKDwBE",
-    authDomain: "motel-manager-247.firebaseapp.com",
-    projectId: "motel-manager-247",
-    storageBucket: "motel-manager-247.appspot.com",
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // cái này mọi người tự gen ra mã ở firebase nha
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "mxxxxxxxxxxxxxxxxxxxxxxxx7",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     messagingSenderId: "93130090266",
     appId: "1:93130090266:web:0dd4601aef59b6745c2ea3"
 });
